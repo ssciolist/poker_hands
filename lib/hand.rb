e535908af06682ed41396c41f325303d41434495
Megan Arellano <ssciolist@gmail.com>
2018-08-21 06:37:30 -0600

    failing card number sort test

--- lib/hand.rb
+++ lib/hand.rb
@@ -3,7 +3,20 @@ class Hand
               :suits
 
   def initialize(card_array)
-    @numbers = card_array.map {|card| card[0]}
+    @numbers = card_num_sort(card_array.map {|card| card[0]})
     @suits = card_array.map {|card| card[1]}
+  end
+
+  def card_num_sort(card_numbers_array)
+    card_numbers_array.map do |number|
+      number.to_i
+    end
+  end
+
+  def face_card_values
+    {"J": 11,
+     "Q": 12,
+     "K": 13,
+     "A": 14}
   end
 end
